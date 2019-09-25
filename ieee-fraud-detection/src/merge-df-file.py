eadcb89399e3280c1e85a37b0ef6b82688aa43b0
thuan <thuanvh@gmail.com>
2019-09-25 17:17:58 +0700

    add field

--- ieee-fraud-detection/src/merge-df-file.py
+++ ieee-fraud-detection/src/merge-df-file.py
@@ -57,6 +57,31 @@ attrs = ['TransactionDT','TransactionAmt','ProductCD',
     'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12', 'C13', 'C14',
     'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'D11', 'D12', 'D13', 'D14', 'D15',
     'M1', 'M2', 'M3', 'M4', 'M5', 'M6', 'M7', 'M8', 'M9',
+    'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13', 'V14', 'V15', 
+    'V16', 'V17', 'V18', 'V19', 'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'V29',
+    'V30', 'V31', 'V32', 'V33', 'V34', 'V35', 'V36', 'V37', 'V38', 'V39', 'V40', 'V41', 'V42', 'V43',
+    'V44', 'V45', 'V46', 'V47', 'V48', 'V49', 'V50', 'V51', 'V52', 'V53', 'V54', 'V55', 'V56', 'V57',
+    'V58', 'V59', 'V60', 'V61', 'V62', 'V63', 'V64', 'V65', 'V66', 'V67', 'V68', 'V69', 'V70', 'V71',
+    'V72', 'V73', 'V74', 'V75', 'V76', 'V77', 'V78', 'V79', 'V80', 'V81', 'V82', 'V83', 'V84', 'V85',
+    'V86', 'V87', 'V88', 'V89', 'V90', 'V91', 'V92', 'V93', 'V94', 'V95', 'V96', 'V97', 'V98', 'V99',
+    'V100', 'V101', 'V102', 'V103', 'V104', 'V105', 'V106', 'V107', 'V108', 'V109', 'V110', 'V111', 'V112', 'V113', 
+    'V114', 'V115', 'V116', 'V117', 'V118', 'V119', 'V120', 'V121', 'V122', 'V123', 'V124', 'V125', 'V126', 'V127', 
+    'V128', 'V129', 'V130', 'V131', 'V132', 'V133', 'V134', 'V135', 'V136', 'V137', 'V138', 'V139', 'V140', 'V141', 
+    'V142', 'V143', 'V144', 'V145', 'V146', 'V147', 'V148', 'V149', 'V150', 'V151', 'V152', 'V153', 'V154', 'V155', 
+    'V156', 'V157', 'V158', 'V159', 'V160', 'V161', 'V162', 'V163', 'V164', 'V165', 'V166', 'V167', 'V168', 'V169',
+    'V170', 'V171', 'V172', 'V173', 'V174', 'V175', 'V176', 'V177', 'V178', 'V179', 'V180', 'V181', 'V182', 'V183',
+    'V184', 'V185', 'V186', 'V187', 'V188', 'V189', 'V190', 'V191', 'V192', 'V193', 'V194', 'V195', 'V196', 'V197',
+    'V198', 'V199', 'V200', 'V201', 'V202', 'V203', 'V204', 'V205', 'V206', 'V207', 'V208', 'V209', 'V210', 'V211',
+    'V212', 'V213', 'V214', 'V215', 'V216', 'V217', 'V218', 'V219', 'V220', 'V221', 'V222', 'V223', 'V224', 'V225',
+    'V226', 'V227', 'V228', 'V229', 'V230', 'V231', 'V232', 'V233', 'V234', 'V235', 'V236', 'V237', 'V238', 'V239',
+    'V240', 'V241', 'V242', 'V243', 'V244', 'V245', 'V246', 'V247', 'V248', 'V249', 'V250', 'V251', 'V252', 'V253',
+    'V254', 'V255', 'V256', 'V257', 'V258', 'V259', 'V260', 'V261', 'V262', 'V263', 'V264', 'V265', 'V266', 'V267',
+    'V268', 'V269', 'V270', 'V271', 'V272', 'V273', 'V274', 'V275', 'V276', 'V277', 'V278', 'V279', 'V280', 'V281',
+    'V282', 'V283', 'V284', 'V285', 'V286', 'V287', 'V288', 'V289', 'V290', 'V291', 'V292', 'V293', 'V294', 'V295',
+    'V296', 'V297', 'V298', 'V299', 'V300', 'V301', 'V302', 'V303', 'V304', 'V305', 'V306', 'V307', 'V308', 'V309',
+    'V310', 'V311', 'V312', 'V313', 'V314', 'V315', 'V316', 'V317', 'V318', 'V319', 'V320', 'V321', 'V322', 'V323',
+    'V324', 'V325', 'V326', 'V327', 'V328', 'V329', 'V330', 'V331', 'V332', 'V333', 'V334', 'V335', 'V336', 'V337',
+    'V338', 'V339', 
     ]
 
 # Correlation ('V300','V309','V111','V124','V106','V125','V315','V134','V102','V123','V316','V113',
@@ -71,7 +96,20 @@ train = train[attrs]
 test = test[attrs]
 bigx = train[attrs].append(test[attrs])
 len_train = len(train)
-#del train, test
+
+c1mean = train['card1'].mean()
+c1max = train['card1'].max()
+c1min = train['card1'].min()
+c2max = train['card2'].max()
+c2min = train['card2'].min()
+addr1max = train['addr1'].max()
+addr1min = train['addr1'].min()
+dist1max = train['dist1'].max()
+dist1min = train['dist1'].min()
+dist2max = train['dist2'].max()
+dist2min = train['dist2'].min()
+del test
+del train
 gc.collect()
 
 for a in attrs:
@@ -90,33 +128,28 @@ bigx = bigx.drop('TransactionDT', axis = 1)
 print(bigx.columns.values.tolist())
 
 #card1
-c1mean = train['card1'].mean()
-c1max = train['card1'].max()
-c1min = train['card1'].min()
 bigx['card1_mean'] = (bigx['card1'] - c1mean)/(c1max - c1min)
 #sea.distplot(bigx['card1_mean'])
 bigx = bigx.drop('card1', axis = 1)
 print(bigx.columns.values.tolist())
 
 #card2
-c2max = train['card2'].max()
-c2min = train['card2'].min()
 bigx['card2_fill'] = bigx['card2'].map(lambda x : np.random.randint(c2min,c2max) if pd.isnull(x) else x)
 
-sea.distplot(bigx['card2_fill'])
+#sea.distplot(bigx['card2_fill'])
 bigx = bigx.drop('card2', axis = 1)
 print(bigx.columns.values.tolist())
 
 #card3
 sea.countplot(bigx['card3'])
-train['card3'].value_counts()
+#train['card3'].value_counts()
 bigx['card3_fill'] = bigx['card3'].map(lambda x : 150.0 if pd.isnull(x) else x)
 bigx['card3_fill'].value_counts()
 bigx = bigx.drop('card3', axis = 1)
 print(bigx.columns.values.tolist())
 
 #card4
-train['card4'].value_counts()
+#train['card4'].value_counts()
 bigx['card4_fill'] = bigx['card4'].map(lambda x : 'Unknown' if pd.isnull(x) else x)
 bigx = encodeLabel(bigx, 'card4_fill')
 bigx['card4_fill'].value_counts()
@@ -125,14 +158,14 @@ print(bigx.columns.values.tolist())
 
 #card5
 sea.countplot(bigx['card5'])
-train['card5'].value_counts()
+#train['card5'].value_counts()
 bigx['card5_fill'] = bigx['card5'].map(lambda x : 226.0 if pd.isnull(x) else x)
 bigx['card5_fill'].value_counts()
 bigx = bigx.drop('card5', axis = 1)
 print(bigx.columns.values.tolist())
 
 #card6
-train['card6'].value_counts()
+#train['card6'].value_counts()
 bigx['card6_fill'] = bigx['card6'].map(lambda x : 'Unknown' if pd.isnull(x) else x)
 bigx = encodeLabel(bigx, 'card6_fill')
 bigx['card6_fill'].value_counts()
@@ -140,37 +173,33 @@ bigx = bigx.drop('card6', axis = 1)
 print(bigx.columns.values.tolist())
 
 #addr1
-c2max = train['addr1'].max()
-c2min = train['addr1'].min()
-bigx['addr1_fill'] = bigx['addr1'].map(lambda x : np.random.randint(c2min,c2max) if pd.isnull(x) else x)
+bigx['addr1_fill'] = bigx['addr1'].map(lambda x : np.random.randint(addr1min,addr1max) if pd.isnull(x) else x)
 
-sea.distplot(bigx['addr1_fill'])
+#sea.distplot(bigx['addr1_fill'])
 bigx = bigx.drop('addr1', axis = 1)
 print(bigx.columns.values.tolist())
 
 #addr2
 sea.countplot(bigx['addr2'])
-train['addr2'].value_counts()
+#train['addr2'].value_counts()
 bigx['addr2_fill'] = bigx['addr2'].map(lambda x : 87.0 if pd.isnull(x) else x)
-bigx['addr2_fill'].value_counts()
+#bigx['addr2_fill'].value_counts()
 bigx = bigx.drop('addr2', axis = 1)
 print(bigx.columns.values.tolist())
 
 #dist1
 sea.countplot(bigx['dist1'])
-train['dist1'].value_counts()
-c2max = train['dist1'].max()
-c2min = train['dist1'].min()
-bigx['dist1_fill'] = bigx['dist1'].map(lambda x : np.random.randint(c2min,c2max) if pd.isnull(x) else x)
+#train['dist1'].value_counts()
+
+bigx['dist1_fill'] = bigx['dist1'].map(lambda x : np.random.randint(dist1min,dist1max) if pd.isnull(x) else x)
 bigx = bigx.drop('dist1', axis = 1)
 print(bigx.columns.values.tolist())
 
 #dist2
 sea.countplot(bigx['dist2'])
-train['dist2'].value_counts()
-c2max = train['dist2'].max()
-c2min = train['dist2'].min()
-bigx['dist2_fill'] = bigx['dist2'].map(lambda x : np.random.randint(c2min,c2max) if pd.isnull(x) else x)
+#train['dist2'].value_counts()
+
+bigx['dist2_fill'] = bigx['dist2'].map(lambda x : np.random.randint(dist2min,dist2max) if pd.isnull(x) else x)
 bigx = bigx.drop('dist2', axis = 1)
 print(bigx.columns.values.tolist())
 
@@ -252,6 +281,11 @@ for D_field in D_list:
 M_list = ['M1', 'M2', 'M3', 'M4', 'M5', 'M6', 'M7', 'M8', 'M9']
 for M_field in M_list:
     bigx = encodeOneHot(bigx, M_field, M_field)
+
+
+for i in range(1,340):
+    V_field = 'V' + str(i)
+    bigx[V_field].fillna(-999,inplace=True)
 
 bigx.to_csv("../input/train_final.csv", index = None, header = True)
 
